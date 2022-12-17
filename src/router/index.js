01187191b5e6de225688546ea11610c7dc163a5b
Aron Emmermann <aron.emmermann@student.htw-berlin.de>
2022-12-17 14:55:33 +0100

    navbar klickbar gemacht

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import Finanzen from '@/views/Finanzen'
 
 const routes = [
   {
@@ -9,16 +11,19 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'About',
+    component: AboutView
+  },
+  {
+    path: '/finanzen',
+    name: 'finanzen',
+    component: Finanzen
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
+  linkActiveClass: 'active',
   routes
 })
 
---
+++ src/views/Finanzen.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Willkommen, hier finden Sie Ihre Finanzübersicht:)</h1>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Finanzen'
+}
+</script>
+
+<style scoped>
+
+</style>
