f24714ba602c4b7582b24fbaf13e7f2a4f3048da
Ekaterina Borovskaya <Ekaterina.Borovskaya@Student.HTW-Berlin.de>
2023-01-09 00:00:09 +0100

    Tabelle existiert ? Test

--- tests/unit/geld-form.spec.js
+++ tests/unit/geld-form.spec.js
@@ -1,5 +1,8 @@
 import { mount } from '@vue/test-utils'
 import GeldForm from '@/components/GeldForm'
+import AboutView from "@/views/AboutView.vue";
+import AusgabeTabelle from "@/components/AusgabeTabelle.vue";
+import HomeView from "@/views/HomeView.vue";
 
 describe('Testing GeldForm.vue', () => {
   it('soll Formular nur zeigen wenn der Button gedrückt wird', () => {
@@ -9,4 +12,13 @@ describe('Testing GeldForm.vue', () => {
     // then
     expect(wrapper.find('#exampleModalLabel').classes()).not.toContain('show')
   })
+
+  it('should have create a form', () => {
+    //when
+    const wrapper = mount(HomeView)
+
+    //then
+    const geldForm = wrapper.findComponent(GeldForm)
+    expect(geldForm.exists()).toBeTruthy()
+  });
 })
